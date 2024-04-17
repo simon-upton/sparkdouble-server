be7b0c989cde71f1aa4d89501569255bae58ae43
Simon Upton <66577371+simon-upton@users.noreply.github.com>
2024-04-17 00:08:41 -0700

    Add Express route and example image embed

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,43 @@
 import "dotenv/config";
-import { Client, Events, GatewayIntentBits } from "discord.js";
+import {
+  Client,
+  EmbedBuilder,
+  Events,
+  GatewayIntentBits,
+  TextChannel,
+} from "discord.js";
+import express from "express";
+import cors from "cors";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const app = express();
+const port = 3000;
 
 client.on("ready", (c) => {
   console.log(`${c.user.username} is online`);
 });
 
 client.login(process.env.BOT_TOKEN);
+
+app.use(cors(), express.json());
+
+app.post("/card", async (req, res) => {
+  // TODO: remove hardcoded IDs used in example
+  const channel = (await client.channels.cache.get(
+    "1229904974872313946"
+  )) as TextChannel;
+  const hueId = "214496939226431489";
+  const user = await client.users.fetch(hueId).catch(() => null);
+  const username: string = user?.username as string;
+  const avatarURL: string = user?.avatarURL() as string;
+  const exampleEmbed = new EmbedBuilder()
+    .setImage(req.body["imgUrl"])
+    .setFooter({ text: username, iconURL: avatarURL })
+    .setTimestamp();
+  await channel.send({ embeds: [exampleEmbed] });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
