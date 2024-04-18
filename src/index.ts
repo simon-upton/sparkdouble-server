0766d11fa1d3e9f833ad844ee005f58aa07f3cff
Simon Upton <66577371+simon-upton@users.noreply.github.com>
2024-04-17 23:13:16 -0700

    Add dynamic command loading

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import "dotenv/config";
+import fs from "node:fs";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
 import {
   Client,
+  Collection,
   EmbedBuilder,
   Events,
   GatewayIntentBits,
@@ -13,18 +17,75 @@ import leveldown from "leveldown";
 import encode from "encoding-down";
 import crypto from "crypto";
 
+// TODO: reorganize/refactor file contents for clarity
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const db = levelup(encode(leveldown("./secretsdb")));
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-const app = express();
-const port = 3000;
+client.commands = new Collection();
+
+// dynamically load all commands from ./commands
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = (await import(filePath)).command;
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
+});
 
 client.on("ready", (c) => {
   console.log(`${c.user.username} is online`);
 });
 
 client.login(process.env.BOT_TOKEN);
+
+const app = express();
+const port = 3000;
 
 app.use(cors(), express.json());
 
